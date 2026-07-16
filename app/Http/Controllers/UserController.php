2d00bb6ab1e15dbdc9a49955e28832240546768a
James Mateo <mr.jamesmateo22@gmail.com>
2026-07-16 14:59:57 +0800

    Add Verified Email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,12 +9,16 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function store(Request $request){
-       User::create([
+        $user = 
+            User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        return view('pages.createuser');
+    // Magpadala ng verification email
+    $user->sendEmailVerificationNotification();
+
+    return view('pages.createuser');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,30 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use App\Models\User;
 
 Route::get('/signup', function () {
     return view('pages.createuser');
 });
 
 Route::post('/users',[UserController::class,'store'])->name('users.store');
+
+
+Route::get('/email/verify/{id}/{hash}', function ($id, $hash) {
+
+    $user = User::findOrFail($id);
+
+    if (! hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
+        abort(403);
+    }
+
+    if (! $user->hasVerifiedEmail()) {
+        $user->markEmailAsVerified();
+    }
+
+    return "Email Verified!";
+
+})->name('verification.verify');
